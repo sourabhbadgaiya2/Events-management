43746a80a8d2c5b3b3eb5aa8b80b39008dcb4096
sourabhbadgaiya2 <sourabhbadgaiya2@gmail.com>
2025-10-01 16:31:41 +0530

    update code

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://events-management-d3gq.onrender.com",
         changeOrigin: true,
         secure: false,
       },
